6e6985e52aa8c6d9d926ffa92a8badab78f15885
ShakhovaAnastasiz <shakhova.anastasiz@gmail.com>
2026-02-03 17:27:34 +0100

    refactor: Remove unused Auth Project tests from Playwright workflows and clean up test code

--- tests/auth.login.spec.ts
+++ tests/auth.login.spec.ts
@@ -1,10 +1,16 @@
-import {expect } from "@playwright/test";
+import { expect } from "@playwright/test";
 import { testUser } from "../credinals.data";
 import path from "path";
 import { test } from "../fixtures";
 const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 
-test("Login", async ({ app , page}) => {
+test("Login", async ({ app, page }) => {
+  test.skip(
+    !!process.env.CI,
+    "Test is skipped in CI due to the Cloudflare protection.",
+  );
+  // const loginPage = new LoginPage(page);
+
   await page.goto("/auth/login");
   await app.loginPage.performLogin(testUser.email, testUser.password);
   await expect(page, "Incorrect URL").toHaveURL("/account");
