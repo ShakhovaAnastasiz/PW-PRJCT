2477db080d7cc22f1d076aae4ca23c93cde79193
ShakhovaAnastasiz <shakhova.anastasiz@gmail.com>
2026-02-03 17:11:52 +0100

    refactor: Remove unused LoginPage instantiation from auth.login.spec.ts

--- tests/auth.login.spec.ts
+++ tests/auth.login.spec.ts
@@ -5,8 +5,6 @@ import { test } from "../fixtures";
 const authFile = path.join(__dirname, "../playwright/.auth/user.json");
 
 test("Login", async ({ app , page}) => {
-  // const loginPage = new LoginPage(page);
-
   await page.goto("/auth/login");
   await app.loginPage.performLogin(testUser.email, testUser.password);
   await expect(page, "Incorrect URL").toHaveURL("/account");
